188a10a0312d48a2a51ebba5609fe1a128a8d912
Aniekan-Samson <aniekansamsonubong@gmail.com>
2024-04-19 16:32:18 +0100

    \07-postpone-with-args.js

--- practice-for-week-03-timeout-pt2-practice-main/problems/07-postpone-with-args.js
+++ practice-for-week-03-timeout-pt2-practice-main/problems/07-postpone-with-args.js
@@ -1,11 +1,11 @@
 /***********************************************************************
-Write a function, `postponeWithArgs`, that accepts a callback and a delay in 
+Write a function, `postponeWithArgs`, that accepts a callback and a delay in
 milliseconds as arguments. `postponeWithArgs` should return a new function that
-accepts any number of arguments. When the returned function is called, it should 
-invoke the callback after the given delay, passing any arguments it receives to 
+accepts any number of arguments. When the returned function is called, it should
+invoke the callback after the given delay, passing any arguments it receives to
 the callback.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Examples
@@ -24,7 +24,18 @@ slowPrintSum(2, 8); // prints '10' after 500 ms
 
 function postponeWithArgs(cb, delay) {
   // Your code here
+  return function(...arg) {
+    return arg +  setTimeout(function () {
+      cb(...arg)
+    }, delay)
+  }
+
 }
+const printSum = (num1, num2) => console.log(num1 + num2);
+const slowPrintSum = postponeWithArgs(printSum, 2000);
+slowPrintSum(4, 3); // prints '7' after 500 ms
+slowPrintSum(2, 8); // prints '10' after 500 ms; // prints 'Hello Alex!' after 1000 ms // prints 'Hello Alex!' after 1000 ms
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
